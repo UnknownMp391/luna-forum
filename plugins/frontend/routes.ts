b05a8ffa0bdbbf0dde4649b7816c4a421767688b
imsunxinhao <imsunxinhao@163.com>
2026-08-22 23:30:06 +0800

    chore: 重新提交

--- plugins/frontend/routes.ts
+++ plugins/frontend/routes.ts
@@ -15,50 +15,6 @@ export function setupFrontendRoutes(server: FastifyInstance): void {
         root: resolve(dirname(fileURLToPath(import.meta.url)), 'public'),
         prefix: '/static/'
     });
-    server.register(fastifyStatic, {
-        root: resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/@fluentui/web-components/dist'),
-        prefix: '/static/fluentui/',
-        decorateReply: false,
-        maxAge: '30d',
-        immutable: true,
-        cacheControl: true
-    });
-    server.register(fastifyStatic, {
-        root: resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/iconify-icon/dist'),
-        prefix: '/static/fluent-icon/',
-        decorateReply: false,
-        maxAge: '30d',
-        immutable: true,
-        cacheControl: true
-    });
-    server.register(fastifyStatic, {
-        root: resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/tslib'),
-        prefix: '/static/tslib/',
-        decorateReply: false,
-        maxAge: '30d',
-        immutable: true,
-        cacheControl: true
-    });
-    server.register(fastifyStatic, {
-        root: resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/@microsoft/fast-element/dist'),
-        prefix: '/static/fastelement/',
-        decorateReply: false,
-        maxAge: '30d',
-        immutable: true,
-        cacheControl: true
-    });
-    server.register(fastifyStatic, {
-        root: resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/@microsoft/fast-foundation/dist'),
-        prefix: '/static/fastfoundation/',
-        decorateReply: false,
-        maxAge: '30d',
-        immutable: true,
-        cacheControl: true
-    });
-    server.get('/dev/layout-preview', async (_request: FastifyRequest, reply: FastifyReply) => {
-        const html = await renderPage('layout.html', { pagename: 'Dev Layout Preview', posts: [] });
-        return reply.type('text/html').send(html);
-    });
     server.get('/login', async (_request: FastifyRequest, reply: FastifyReply) => {
         const html = await renderPage('login.html', { pagename: '登录', posts: [] });
         return reply.type('text/html').send(html);
